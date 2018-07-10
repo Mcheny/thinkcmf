bf9a607668eaa936a98cd036a82c8b2a950164c8
396557004@qq.com <396557004@qq.com>
2018-07-10 15:20:00 +0800

    a

---
+++ app/web/controller/IndexController.php
@@ -0,0 +1,31 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+namespace app\web\controller;
+use app\portal\model\PortalPostModel;
+use app\portal\service\PostService;
+use cmf\controller\HomeBaseController;
+class IndexController extends HomeBaseController
+{
+    public function index()
+    {
+        $param = $this->request->param();
+
+        $categoryId = $this->request->param('category', 0, 'intval');
+
+        $postService = new PostService();
+        $data        = $postService->adminArticleList($param);
+        $data->appends($param);
+        echo '<pre>';
+        return $this->json( '0', '请求成功',$data->items());
+        echo '这里是前台接口';exit;
+        return $this->fetch(':index');
+    }
+}
