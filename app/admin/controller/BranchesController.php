f72578392bd39783e246d1b8734c5a9eedab1b92
396557004@qq.com <396557004@qq.com>
2018-07-24 15:31:40 +0800

    第一次

---
+++ app/admin/controller/BranchesController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/24
+ * Time: 14:52
+ */
+namespace app\admin\controller;
+
+use cmf\controller\AdminBaseController;
+
+class BranchesController extends AdminBaseController
+{
+    public function _initialize()
+    {
+
+    }
+    public function index(){
+       echo 1111;
+    }
+}
